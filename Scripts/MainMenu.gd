62fb96870690d9db1a5cc2f1dff49fd65b07612c
devklebinho <contato@klebersilva.dev.br>
2026-09-12 20:34:49 -0300

    adjust on path of credit scene

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -7,7 +7,7 @@ func _on_options_pressed() -> void:
 		get_tree().change_scene_to_file("res://Scenes/Options.tscn")
 
 func _on_credits_pressed() -> void:
-	#get_tree().change_scene_to_file("res://Scenes/Options.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
 	print("Imagine Credits here! It will be implemented after")
 	pass # Replace with function body.
 

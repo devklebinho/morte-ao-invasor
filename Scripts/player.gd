be2398c9fad3a83ac7d89c29be69f62c562a6e24
devklebinho <contato@klebersilva.dev.br>
2026-09-22 00:34:24 -0300

    add new scene node + camera

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("MoveLeft", "MoveRight")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
